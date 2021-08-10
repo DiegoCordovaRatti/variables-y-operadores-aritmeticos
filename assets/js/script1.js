1f4b18d71bc385453cb609dfd1cbae4a81039ef3
Diego <d.cordovaratti@gmail.com>
2021-08-09 23:37:09 -0300

    second commit

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -8,8 +8,8 @@ let multiplicacion = n1 * n2 //variable de multiplicación
 let division = (n1 / n2).toFixed(2) //variable de división con decimales fijados en 2 máximo
 let modulo = (n1 % n2) //variable de calculo de modulo entre 2 numeros
 
-document.write(`<p>el resultado de su suma es: <strong> ${suma} </strong></p>`)
-document.write(`<p>el resultado de su resta es: <strong> ${resta} </strong></p>`)
-document.write(`<p>el resultado de su multiplicación es: <strong> ${multiplicacion} </strong></p>`)
-document.write(`<p>el resultado de su división es: <strong> ${division} </strong></p>`)
-document.write(`<p>el resultado de su modulo es: <strong> ${modulo} </strong></p>`)
+document.write(`<p>el resultado de su suma es: <strong> ${suma} </strong></p>`) //se muestra la suma
+document.write(`<p>el resultado de su resta es: <strong> ${resta} </strong></p>`)//se muestra la resta
+document.write(`<p>el resultado de su multiplicación es: <strong> ${multiplicacion} </strong></p>`)//se muestra la multiplicación
+document.write(`<p>el resultado de su división es: <strong> ${division} </strong></p>`)//se muestra la división
+document.write(`<p>el resultado de su modulo es: <strong> ${modulo} </strong></p>`)//se muestra el modulo
